137857d2a20c25d0b7d73a51ba0dddd1966068f2
silviacremascoli <silcremascoli@gmail.com>
2023-05-25 17:33:41 +0200

    Created parent class and child classes

---
+++ app.js
@@ -0,0 +1,63 @@
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._ratings = [];
+    this._isCheckedOut = false;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  getAverageRating() {}
+  toggleCheckOutStatus() {}
+  addRating() {}
+}
+
+class Book extends Media {
+  constructor(author, title, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+}
+
+class Movie extends Media {
+  constructor(director, title, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+
+  get director() {
+    return this._director;
+  }
+
+  get runTime() {
+    return this._runTime;
+  }
+}
+
+class CD extends Media {
+  constructor(artist, title, songs) {
+    super(title);
+    this._artist = artist;
+    this._songs = songs;
+  }
+
+  get artist() {
+    return this._artist;
+  }
+
+  get songs() {
+    return this._songs;
+  }
+}
