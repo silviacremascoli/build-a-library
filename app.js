e3af36a968ea35b7e5423c789d30d333087a62e3
silviacremascoli <silcremascoli@gmail.com>
2023-05-25 18:05:43 +0200

    Updated methods and added instances

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ class Media {
   }
 
   set isCheckedOut(newIsCheckedOut) {
-    if (typeof newIsCheckedOut === "boolean") {
+    if (typeof newIsCheckedOut === boolean) {
       this._isCheckedOut = newIsCheckedOut;
     }
   }
@@ -28,17 +28,17 @@ class Media {
       (acc, currValue) => acc + currValue,
       0
     );
-    return ratingsSum / this.ratings.length;
+    return Math.round(ratingsSum / this.ratings.length);
   }
   toggleCheckOutStatus() {
-    if (isCheckedOut === true) {
-      return isCheckedOut === false;
-    } else {
-      return isCheckedOut === true;
-    }
+    return (this._isCheckedOut = !this._isCheckedOut);
   }
   addRating(rating) {
-    this.ratings.push(rating);
+    if (rating > 0 && rating < 6) {
+      this.ratings.push(rating);
+    } else {
+      console.log("You need to type a number between 1 and 5");
+    }
   }
 }
 
@@ -89,3 +89,24 @@ class CD extends Media {
     return this._songs;
   }
 }
+
+const historyOfEverything = new Book(
+  "Bill Bryson",
+  "A Short History of Nearly Everything",
+  544
+);
+
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie("Jan de Bont", "Speed", 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log(speed.getAverageRating());
